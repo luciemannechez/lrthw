392da6525ad3bb20b21d5db17daf183e57e91cb2
Lucie Mannechez <lucie.mannechez@gmail.com>
2015-04-23 18:09:04 +0200

    Fini les exercices 1 a 15

---
+++ ex7.rb
@@ -0,0 +1,26 @@
+# decrit une chaine de caractrere
+puts "Mary had a little lamb."
+# decrit une autre chaine de caratere
+puts "Its fleece was white as #{'snow'}."
+# encore une
+puts "And everywhere that Mary went."
+# multiplie le caractere . par 10 donc affiche 10 points
+puts "." * 10  # what'd that do?
+
+# toutes ces lignes mettent des lettres dans des variables et a la fin les variables sont additionnees pour former le mot cheeseburger
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
